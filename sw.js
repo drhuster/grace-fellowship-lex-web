60285a8095a6068892363c4b755bdffb2a7542a7
Daniel Huster <dr.huster@gmail.com>
2021-04-30 19:26:56 -0400

    Updated routes

--- sw.js
+++ sw.js
@@ -1,81 +1,81 @@
-importScripts('/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/%3Crepository-name%3E/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/13704a5.js",
-    "revision": "3eee607251b49273c3b53620f0c21c7a"
+    "url": "/%3Crepository-name%3E/_nuxt/06846e7.js",
+    "revision": "f53fcb4da8bb010f16266f5b1cc04438"
   },
   {
-    "url": "/_nuxt/27143a5.js",
-    "revision": "69a401be14a334761c88fe24de8fd65b"
+    "url": "/%3Crepository-name%3E/_nuxt/0c6bb71.js",
+    "revision": "3cf554e1394dba7316e026399e93cb89"
   },
   {
-    "url": "/_nuxt/369a793.js",
-    "revision": "25a1460fe2a73a57f855e32b6c88efb4"
+    "url": "/%3Crepository-name%3E/_nuxt/0f52e5c.js",
+    "revision": "5e93313b340f1123f1b62b149c39bd9a"
   },
   {
-    "url": "/_nuxt/3c55ced.js",
-    "revision": "0b79c1fc181cba737844450ca4dafa3c"
+    "url": "/%3Crepository-name%3E/_nuxt/10ac87b.js",
+    "revision": "a93c5196885b32072bdd2fbaac9f1a59"
   },
   {
-    "url": "/_nuxt/553f7a9.js",
-    "revision": "a534b489920571003c764397c4f009b7"
+    "url": "/%3Crepository-name%3E/_nuxt/1fd36ae.js",
+    "revision": "d0846572280c2d96ef7d2f01f2840eb9"
   },
   {
-    "url": "/_nuxt/6f799b5.js",
-    "revision": "fea17af26b189adba4c209dfe8350e0c"
+    "url": "/%3Crepository-name%3E/_nuxt/2ed7c17.js",
+    "revision": "d97c1843827bcfcfa6e6ad1fed2e9711"
   },
   {
-    "url": "/_nuxt/7b5be29.js",
-    "revision": "f6f5873b848aacee1aa935652da8ecb5"
+    "url": "/%3Crepository-name%3E/_nuxt/35b8721.js",
+    "revision": "869178c44105bab613562615710776bc"
   },
   {
-    "url": "/_nuxt/7eb5e63.js",
-    "revision": "2d01ad2754801769de6df21ff51b700f"
+    "url": "/%3Crepository-name%3E/_nuxt/3ccabf8.js",
+    "revision": "0a0fb027fea9fed7da3e42307b4e4164"
   },
   {
-    "url": "/_nuxt/82414f7.js",
-    "revision": "2f1061525a611df219d584cdec36fa02"
+    "url": "/%3Crepository-name%3E/_nuxt/491a055.js",
+    "revision": "2369764da0cf2a4dc617c7553a566a41"
   },
   {
-    "url": "/_nuxt/85751d1.js",
-    "revision": "ad8f30e832858f73b516a802775aef94"
+    "url": "/%3Crepository-name%3E/_nuxt/5ae511f.js",
+    "revision": "21563b32081c628e1a0a581da614fc46"
   },
   {
-    "url": "/_nuxt/8f33e6f.js",
-    "revision": "23664061bffc3e59607f7b65f9a3d929"
+    "url": "/%3Crepository-name%3E/_nuxt/6d562d2.js",
+    "revision": "02f8836751a4ae5370155376e5815567"
   },
   {
-    "url": "/_nuxt/a771aae.js",
-    "revision": "ad8ebf662fbf48ef51773ba1a43946ac"
+    "url": "/%3Crepository-name%3E/_nuxt/81383e0.js",
+    "revision": "05c184790135fc6afee0a9b3c6d7eade"
   },
   {
-    "url": "/_nuxt/bdbb268.js",
-    "revision": "94351f2ba25fb4651ebe0f00c0552475"
+    "url": "/%3Crepository-name%3E/_nuxt/8be36bc.js",
+    "revision": "5f25cdd45217a5e8d5c6e7bdb1426423"
   },
   {
-    "url": "/_nuxt/bf72765.js",
-    "revision": "f87d2479bb33c530e0999773d0f9304b"
+    "url": "/%3Crepository-name%3E/_nuxt/962e4c3.js",
+    "revision": "863f2d23d870a8145cef4451a653b403"
   },
   {
-    "url": "/_nuxt/d6dbb15.js",
-    "revision": "3d426fb13b5b924834e099a50593d58f"
+    "url": "/%3Crepository-name%3E/_nuxt/a0dbc65.js",
+    "revision": "880cc913f84008749cbcbb704b15fab2"
   },
   {
-    "url": "/_nuxt/d9a6e3d.js",
-    "revision": "8f7126eb1e70efe3945273046dba98d6"
+    "url": "/%3Crepository-name%3E/_nuxt/b556d80.js",
+    "revision": "4489834a8afc30ffa48302a1aa1743d1"
   },
   {
-    "url": "/_nuxt/db054cb.js",
-    "revision": "d86a81507b640ad99066cc0b80fe8b54"
+    "url": "/%3Crepository-name%3E/_nuxt/db37be0.js",
+    "revision": "18519e5bcecaf9f21fc51b19624d7a13"
   },
   {
-    "url": "/_nuxt/e257ded.js",
-    "revision": "d3ecf828a0ada5a17c3049c3a0d14010"
+    "url": "/%3Crepository-name%3E/_nuxt/df34612.js",
+    "revision": "c624ef58becf73d011c1708d3b45f61c"
   },
   {
-    "url": "/_nuxt/e953cb2.js",
-    "revision": "93f77a90dca7840cda55d6a369d8c7d1"
+    "url": "/%3Crepository-name%3E/_nuxt/fcb3021.js",
+    "revision": "03eb2a1c0cb57c814715e65ddd24dcb5"
   }
 ], {
   "cacheId": "grace-fellowship-lex",
@@ -86,6 +86,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/%3Crepository-name%3E/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/%3Crepository-name%3E/.*'), workbox.strategies.networkFirst({}), 'GET')
